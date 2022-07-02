5b521b89ec851c14eb035675c4e103c44a28b1a6
LAPTOP-TSCH4DV9\haPPy <soumya@cipio.ai>
2022-07-03 02:40:08 +0530

    Code Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'classytrips';
+  images = [
+    { source: './../assets/images/bg.jpg' },
+    { source: './../assets/images/bg.jpg' },
+  ];
+
+  constructor(config: NgbCarouselConfig, private httpClient: HttpClient) {
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.httpClient.get('https://soumyasidharth.pythonanywhere.com/name').subscribe((res: any) => {
+      console.log(res);
+    })
+  }
 }
