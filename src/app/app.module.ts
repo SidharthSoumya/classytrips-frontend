aeeba79016346c2063765e9f5fea3be541b16f42
LAPTOP-TSCH4DV9\haPPy <soumya@cipio.ai>
2022-07-04 13:54:01 +0530

    Angular 14 Syntax Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
+import { HeaderComponent } from './layouts/header/header.component';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, NgbModule]
+  declarations:[],
+  imports: []
 })
 export class AppModule {}
