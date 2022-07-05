975d8328aea3986d246c5d7f84167faa1d98a5b5
LAPTOP-TSCH4DV9\haPPy <soumya@cipio.ai>
2022-07-05 23:34:23 +0530

    Code Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OurServicesComponent } from './layouts/our-services/our-services.component';
 import { LandingPageComponent } from './layouts/landing-page/landing-page.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { HeaderComponent } from './layouts/header/header.component';
@@ -8,7 +9,7 @@ import { Router, RouterModule } from '@angular/router';
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, RouterModule, FooterComponent],
+  imports: [HeaderComponent, RouterModule, FooterComponent, LandingPageComponent, OurServicesComponent],
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
